b7fb19fa198cb3bfcfa00972aeb3a95812933e38
Rodrigo Otavio van den Berg Maia <rodrigo@ladodireito.com>
2015-09-16 19:34:52 +0000

    Move partials to propper dir
    Creates a dir to each section of pdsi items.

--- app/models/dsei.rb
+++ app/models/dsei.rb
@@ -5,8 +5,12 @@ class Dsei < ActiveRecord::Base
   validates :name, length: { maximum: 255 }, presence: true, uniqueness: true
   validates :sesai_id,  numericality: true, uniqueness: true
 
-  def base_polos_with_children
-    base_polos.includes(:pdsi_base_polo_datum, :emsis, villages: [physiographic_data: [:physiographic_data_languages]])
+  def base_polos_with_emsis
+    base_polos.includes(:pdsi_base_polo_datum, :emsis)
+  end
+
+  def base_polos_with_physiographic_data
+    base_polos.includes(villages: [physiographic_data: [:physiographic_data_languages]])
   end
 
   def base_polos_with_service_networks
