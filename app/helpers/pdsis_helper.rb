8b3db0a650046709bc9acdd94fcef37c95d1e5d6
pedromartos <pedro@devlandia.net>
2015-09-23 15:21:44 +0000

    3.2.g CAPAis - CRUD

--- app/helpers/pdsis_helper.rb
+++ app/helpers/pdsis_helper.rb
@@ -8,7 +8,7 @@ module PdsisHelper
     three = [{ name: 'Caracterização do DSEI', path: pdsis_path('caracterizacao-do-dsei')}]
     bread = [{ name: section_name(section), path: pdsis_path(section: section.gsub(/_/, '-'))}]
 
-    if %w"mapa dados_fisiograficos capacidade_instalada rede_servicos infraestrutura_edificacoes infraestrutura_saneamento".include? section
+    if %w"mapa dados_fisiograficos destinos capacidade_instalada rede_servicos infraestrutura_edificacoes infraestrutura_saneamento capais".include? section
       three + bread
     else
       bread
@@ -19,13 +19,17 @@ module PdsisHelper
     {
       introducao: 'Introdução',
       processo_de_construcao_do_pdsi: 'O Processo de construção',
+      # 3
       caracterizacao_do_dsei: 'Caracterização do DSEI',
       mapa: 'Mapa',
       dados_fisiograficos: 'Dados fisiográficos',
+      destinos: 'Meios de Transporte',
       capacidade_instalada: 'Capacidade Instalada',
       rede_servicos: 'Rede de serviços',
       infraestrutura_edificacoes: 'Infraestrutura de saúde edificações',
       infraestrutura_saneamento:  'Infraestrutura de saúde - Saneamento',
+      capais:        'CAPAIs',
+      # 4
       analise_situacional:        'Análise situacional',
       indicadores_saude_indigena: 'Indicadores estratégicos para a saúde indígena',
     }[section.to_sym]
