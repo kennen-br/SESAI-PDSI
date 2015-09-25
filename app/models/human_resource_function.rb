b9c3fd9c1ac0fe9b8ebd048a537e7e78f9583c24
pedromartos <pedro@devlandia.net>
2015-09-25 13:58:08 +0000

    Validates presence of HumanResourceFunction when creating PdsiHumanResource

--- app/models/human_resource_function.rb
+++ app/models/human_resource_function.rb
@@ -1,5 +1,5 @@
 class HumanResourceFunction < ActiveRecord::Base
   auditable
 
-  has_many  :pdsi_human_resources
+  has_many  :pdsi_human_resources, inverse_of: :human_resource_function
 end
--- app/models/pdsi_human_resource.rb
+++ app/models/pdsi_human_resource.rb
@@ -2,5 +2,7 @@ class PdsiHumanResource < ActiveRecord::Base
   auditable
 
   belongs_to :pdsi
-  belongs_to :human_resource_function
+  belongs_to :human_resource_function, inverse_of: :pdsi_human_resources
+
+  validates :human_resource_function, :pdsi, presence: true
 end
