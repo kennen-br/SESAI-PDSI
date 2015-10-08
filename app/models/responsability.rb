08cb4edb16cde2c65593980f56d7efdad42f49a2
Rodrigo Otavio van den Berg Maia <rodrigo@ladodireito.com>
2015-10-08 20:46:31 +0000

    Split base fields into a partial

--- app/models/responsability.rb
+++ app/models/responsability.rb
@@ -1,12 +1,15 @@
 class Responsability < ActiveRecord::Base
-  include Parentable
   auditable
 
   belongs_to :pdsi
   belongs_to :result
   belongs_to :person
   belongs_to :responsability_level
+  belongs_to :parent, class_name: 'Responsability'
 
   has_many  :corresponsabilities
   accepts_nested_attributes_for :corresponsabilities, reject_if: :all_blank, allow_destroy: true
+
+  has_many  :children,  class_name: 'Responsability', foreign_key: :parent_id
+  accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
 end
