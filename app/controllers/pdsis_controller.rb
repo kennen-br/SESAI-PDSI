1d0e48009188a503e53284e1883bb5b612b4248b
pedromartos <pedro@devlandia.net>
2015-10-27 19:35:46 +0000

    Form, Controller and Locales for BudgetForecast

--- app/controllers/pdsis_controller.rb
+++ app/controllers/pdsis_controller.rb
@@ -159,7 +159,8 @@ private
       ],
       pdsi_attached_files_attributes: [
         :id, :name, :file
-      ]
+      ],
+      budget_forecasts_attributes: [:id, :reference_forecast, :budget_forecast, :initial_allocation_2016, :dsei_forecast_2016, :revised_allocation_2016, :initial_forecast_2017, :dsei_forecast_2017, :revised_forecast_2017, :initial_forecast_2018, :dsei_forecast_2018, :revised_forecast_2018, :initial_forecast_2019, :dsei_forecast_2019, :revised_forecast_2019]
     )
   end
 end
