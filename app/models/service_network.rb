40cb07f9678d4db89f24347560c616e1c88dcea3
Marcelo Bressan <marcelo.bressan@kennen.com.br>
2015-10-30 17:34:18 -0200

    adequações de tela e ajustes

--- app/models/service_network.rb
+++ app/models/service_network.rb
@@ -10,5 +10,5 @@ class ServiceNetwork < ActiveRecord::Base
   has_many  :health_specializeds
   accepts_nested_attributes_for :health_specializeds, reject_if: :all_blank, allow_destroy: true
 
-  validates :city_name,  uniqueness: { scope: [:base_polo, :pdsi], message: 'Cidade já cadastrada no polo base' }
+  validates :city_name,  uniqueness: { scope: [:base_polo, :pdsi], message: 'Município já cadastrado no polo base' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20151024162730) do
-  
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -519,6 +519,9 @@ ActiveRecord::Schema.define(version: 20151024162730) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "workspace"
+    t.string "location"
+    t.string "bond" 
   end
 
   add_index "people", ["dsei_id"], name: "index_people_on_dsei_id", using: :btree
