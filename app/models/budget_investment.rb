174aee48127a459646a2bd090c8272538db14f37
Helio Oliveira <hheelliioo@live.com>
2015-12-04 17:28:40 -0200

    relations for new investmensts_iems

--- app/models/budget_investment.rb
+++ app/models/budget_investment.rb
@@ -4,4 +4,7 @@ class BudgetInvestment < ActiveRecord::Base
   def self.find_by_investment(id)
     where(investment_id: id)
   end
+
+  has_many  :investment_items
+  accepts_nested_attributes_for :investment_items, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,6 +2,6 @@ class Investment < ActiveRecord::Base
   include Parentable
   auditable
   belongs_to :parent, class_name: 'Investment'
-  has_many :budget_investments
+  has_one :budget_investments
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
 end
