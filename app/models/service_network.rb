ce42cbae7558a9f9b4bcfe979eefaa9724d9b15a
Helio Oliveira <hheelliioo@live.com>
2016-01-18 17:12:25 -0200

    fix Delete city references 3.2D

--- app/models/service_network.rb
+++ app/models/service_network.rb
@@ -4,7 +4,7 @@ class ServiceNetwork < ActiveRecord::Base
   belongs_to :base_polo
   belongs_to :pdsi
 
-  has_many  :service_network_cities
+  has_many  :service_network_cities, dependent: :destroy
   accepts_nested_attributes_for :service_network_cities,  reject_if: :all_blank,  allow_destroy: true
 
   validates :city_name,  uniqueness: { scope: [:base_polo, :pdsi], message: 'Município já cadastrado no polo base' }
--- app/models/service_network_city.rb
+++ app/models/service_network_city.rb
@@ -3,9 +3,9 @@ class ServiceNetworkCity < ActiveRecord::Base
 
   belongs_to :service_network
 
-  has_many  :health_establishments
+  has_many  :health_establishments, dependent: :destroy
   accepts_nested_attributes_for :health_establishments, reject_if: :all_blank, allow_destroy: true
 
-  has_many  :health_specializeds
+  has_many  :health_specializeds, dependent: :destroy
   accepts_nested_attributes_for :health_specializeds, reject_if: :all_blank, allow_destroy: true
 end
