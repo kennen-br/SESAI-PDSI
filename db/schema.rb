7610dcd6b92f56f53d4fc062f7a5a0e8fbf8e3fe
Helio Oliveira <hheelliioo@live.com>
2016-01-20 15:47:56 -0200

    Add population to BasePolo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120161038) do
+ActiveRecord::Schema.define(version: 20160120172458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160120161038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "city_name"
+    t.integer  "population"
   end
 
   add_index "base_polos", ["dsei_id"], name: "index_base_polos_on_dsei_id", using: :btree
