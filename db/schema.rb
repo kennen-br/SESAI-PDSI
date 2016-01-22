7987c7431d3b4f52665ed1a2f30553e1ee3e3cc7
Helio Oliveira <hheelliioo@live.com>
2016-01-22 12:15:37 -0200

    Add name to capais

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120172458) do
+ActiveRecord::Schema.define(version: 20160122141341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 20160120172458) do
     t.integer  "host_capacity"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "name"
   end
 
   add_index "capais", ["pdsi_id"], name: "index_capais_on_pdsi_id", using: :btree
