2cdeb3b162fb9bc8b33a58d1042f47c08f204d5e
Helio Oliveira <hheelliioo@live.com>
2016-01-22 11:17:32 -0200

    Add ability to erase Capai

--- app/models/capai.rb
+++ app/models/capai.rb
@@ -3,6 +3,6 @@ class Capai < ActiveRecord::Base
 
   belongs_to :pdsi
 
-  has_many  :capai_villages
+  has_many :capai_villages, dependent: :destroy
   accepts_nested_attributes_for :capai_villages, reject_if: :all_blank, allow_destroy: true
 end
