90770ca3021de7c8d79f0ca7e2da8175abebad20
Helio Oliveira <hheelliioo@live.com>
2016-01-29 13:32:25 -0200

    Remove email validation and include headquarters to permited params

--- app/models/demographic_data.rb
+++ app/models/demographic_data.rb
@@ -6,7 +6,6 @@ class DemographicData < ActiveRecord::Base
   validates :extensao_territorial,  length: { maximum: 255 }, presence: false
   validates :municipio_sede,        length: { maximum: 255 }, presence: false
   validates :endereco,              length: { maximum: 255 }, presence: false
-  validates :email,                 email: true
 
   # TODO: Não dá erro quando salva não número, mas quando salva número passa.
   validates :numero_municipios,       numericality: { only_integer: true, allow_nil: true }
