00321c1c33feb7f20ec4c78c8ed22f164f868dbf
Helio Oliveira <hheelliioo@live.com>
2016-03-21 18:54:22 -0300

    Relation bond with bond_type

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,4 +9,22 @@ class Person < ActiveRecord::Base
   validates :name, length: { mininum: 3, maximum: 255 }, uniqueness: { scope: :dsei }, presence: true
 
   scope :sesai_people, -> { where(is_sesai_central: true) }
+
+  def bond_selection
+    {
+      'CONVENIADA' => 'CONVÊNIO',
+      'PROJETO MAIS MÉDICOS - PMM' => 'PMM/PROVAB',
+      'ATIVO PERMANENTE' => 'SESAI/DSEI',
+      'CEDIDO MUNICÍPIO' => 'CEDIDO MUNICÍPIO',
+      'CEDIDO ESTADO' => 'CEDIDO ESTADO',
+      'CEDIDO ÓRGÃO FEDERAL' => 'CEDIDO ÓRGÃO FEDERAL',
+      'CEDIDO SUS/LEI Nº 8.270' => 'SESAI/DSEI',
+      'CONTRATO TEMPORÁRIO DA UNIÃO' => 'SESAI/DSEI',
+      'EXERC.§7º ART. 93 DA LEI Nº 8.112' => 'SESAI/DSEI',
+      'EXERCÍCIO DESCENTRALIZADO DE CARREIRA' => 'SESAI/DSEI',
+      'NOMEADO CARGO COMISSIONADO' => 'SESAI/DSEI',
+      'REQUISITADO DE OUTROS ÓRGÃOS' => 'SESAI/DSEI',
+      'TERCEIRIZADOS' => 'TERCEIRIZADOS'
+    }
+  end
 end
