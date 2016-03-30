79c0bdd8cea613342f7f550791de6ad883d25c5e
Helio Oliveira <hheelliioo@live.com>
2016-03-30 11:38:57 -0300

    Validate demographic_datas

--- app/models/physiographic_data.rb
+++ app/models/physiographic_data.rb
@@ -9,4 +9,31 @@ class PhysiographicData < ActiveRecord::Base
 
   has_many :physiographic_data_ethnicities
   accepts_nested_attributes_for :physiographic_data_ethnicities, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :m_1,
+                        :pt_fluency,
+                        :m_1,
+                        :m_1_4,
+                        :m_5_9,
+                        :m_10_49,
+                        :m_50_59,
+                        :m_60,
+                        :w_1,
+                        :w_1_4,
+                        :w_5_9,
+                        :w_10_49,
+                        :w_50_59,
+                        :w_60,
+                        :m_1,
+                        :m_1_4,
+                        :m_5_9,
+                        :m_10_49,
+                        :m_50_59,
+                        :m_60,
+                        :w_1,
+                        :w_1_4,
+                        :w_5_9,
+                        :w_10_49,
+                        :w_50_59,
+                        :w_60
 end
