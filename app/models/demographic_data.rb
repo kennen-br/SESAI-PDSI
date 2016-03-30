38d94ecb317c0b81c8673c93d2a4926a290a83c6
Helio Oliveira <hheelliioo@live.com>
2016-03-30 11:38:00 -0300

    Validate demographic_data

--- app/models/demographic_data.rb
+++ app/models/demographic_data.rb
@@ -8,18 +8,10 @@ class DemographicData < ActiveRecord::Base
   validates :municipio_sede,        length: { maximum: 255 }, presence: false
   validates :endereco,              length: { maximum: 255 }, presence: false
 
-  # TODO: Não dá erro quando salva não número, mas quando salva número passa.
-  validates :numero_municipios,       numericality: { only_integer: true, allow_nil: true }
-  validates :populacao_indigena,      numericality: { only_integer: true, allow_nil: true }
-  validates :numero_polos_base,       numericality: { only_integer: true, allow_nil: true }
-  validates :numero_ubsi,             numericality: { only_integer: true, allow_nil: true }
-  validates :numero_regioes_de_saude, numericality: { only_integer: true, allow_nil: true }
-  validates :numero_casais,           numericality: { only_integer: true, allow_nil: true }
-  validates :numero_aldeias,          numericality: { only_integer: true, allow_nil: true }
-  validates :numero_familias,         numericality: { only_integer: true, allow_nil: true }
-  
-  def extensao_territorial_km
-    number_to_human(extensao_territorial.to_f, precision: 5, separator: '.', units: {unit: "metros", thousand: 'km²'}) 
-  end
+  validates_presence_of :numero_sede_polos_base, :numero_ubsi
 
+  def extensao_territorial_km
+    number_to_human(extensao_territorial.to_f, precision: 5, separator: '.',
+                                               units: { unit: 'metros', thousand: 'km²' })
+  end
 end
